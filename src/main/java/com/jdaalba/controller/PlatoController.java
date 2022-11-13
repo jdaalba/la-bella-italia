ed1aab179fcdcd3c280180be872216a10cc5ab3e
jdaalba <jdaalba@gmail.com>
2022-11-13 10:19:39 +0100

    Modificación de platos

--- src/main/java/com/jdaalba/controller/PlatoController.java
+++ src/main/java/com/jdaalba/controller/PlatoController.java
@@ -4,7 +4,7 @@ import com.jdaalba.constant.Categoria;
 import com.jdaalba.constant.Etiqueta;
 import com.jdaalba.entity.Plato;
 import com.jdaalba.service.PlatosService;
-import java.util.Arrays;
+import javax.validation.constraints.NotBlank;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,17 +24,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Slf4j
 public record PlatoController(PlatosService service) {
 
-//  @PostMapping
-//  public String registrarPlato(@ModelAttribute Plato plato, Model model) {
-//    log.info("Creando plato: {}", plato);
-//    service.salvar(plato);
-//    return "redirect:/platos";
-//  }
-
   @PostMapping
   @ResponseBody
   public ResponseEntity<Void> salvarPlato(@RequestBody Plato plato) {
     log.info("Creando plato: {}", plato);
+    service.salvar(plato);
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+
+  @PutMapping("/{id}")
+  @ResponseBody
+  public ResponseEntity<Void> modificarPlato(
+      @PathVariable @NotBlank String id, @RequestBody Plato plato
+  ) {
+    log.info("Modificando plato: {}", plato);
+    assert id.equals(plato.getId());
     service.salvar(plato);
     return ResponseEntity.status(HttpStatus.CREATED).build();
   }
@@ -56,15 +60,11 @@ public record PlatoController(PlatosService service) {
     return "admin/lista-platos";
   }
 
-  @Deprecated
-  @GetMapping("/modificar/{id}")
-  public String modficar(Model model, @PathVariable("id") String id) {
-    log.info("Modificando plato con id {}", id);
-    // todo: implementar un 404 correcto
-    model.addAttribute("plato", service.buscar(id).orElseThrow());
-    model.addAttribute("categorias", Categoria.values());
-    model.addAttribute("etiquetas", Etiqueta.values());
-    return "admin/platos";
+  @GetMapping("/{id}")
+  @ResponseBody
+  public Plato buscar(@PathVariable("id") String id) {
+    log.info("Buscando plato con id {}", id);
+    return service.buscar(id).orElseThrow();
   }
 
   @GetMapping("/borrar/{id}")
